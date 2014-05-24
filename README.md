c1ee1f6415cf4b95a50e76376befda9da8402cb2
Tom de Grunt <tom@degrunt.nl>
2014-05-25 00:25:52 +0200

    Initial import

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-broccoli-ember-i18n-precompile
-==============================
+# broccoli-ember-i18n-precompile
 
-Opinionated broccoli ember-i18n precompilation plugin
+WARNING: Work in progress
+
+The broccoli-ember-i18n-precompile plugin precompiles `.json` files with translations.
+
+This is a very opinionated plugin, it assumes a top level folder with the language name, 
+with individual folders and JSON files beneath them. So something like this:
+
+It assumes you will load the translations at initialization time.
+
+```
+app
+|- translations
+|  |- en
+|  |  |- users
+|  |  |  |- index.json
+|  |- de
+|  |  |- users
+|  |  |  |- index.json
+```
+
+## Installation
+
+```bash
+npm install --save-dev broccoli-ember-i18n-precompile
+```
+
+## Usage
+
+```js
+var emberI18nPrecompile = require('broccoli-ember-i18n-precompile');
+
+var outputTree = emberI18nPrecompile(inputTrees, options)
+```
+
+* **`inputTree`**: Trees that act as the source
+
+* **`options`**: A hash of options.
+
+### Example
+
+```js
+var appTranslations = emberI18nPrecompile("app/translations", {outputFolder: 'assets/translations'});
+```
---
+++ index.js
@@ -0,0 +1,95 @@
+var fs = require('fs')
+var path = require('path')
+var mkdirp = require('mkdirp')
+var quickTemp = require('quick-temp')
+var helpers = require('broccoli-kitchen-sink-helpers')
+var Writer = require('broccoli-writer');
+var handlebars = require('handlebars');
+
+function setValue(object, path, value) {
+  var a = path.split('.');
+  var o = object;
+  for (var i = 0; i < a.length - 1; i++) {
+      var n = a[i];
+      if (n in o) {
+          o = o[n];
+      } else {
+          o[n] = {};
+          o = o[n];
+      }
+  }
+  o[a[a.length - 1]] = value;
+}
+
+
+module.exports = Precompiler
+Precompiler.prototype = Object.create(Writer.prototype)
+Precompiler.prototype.constructor = Precompiler
+
+function Precompiler (inputTree, options) {
+  if (!(this instanceof Precompiler)) return new Precompiler(inputTree, options)
+  this.inputTree = inputTree
+  for (var key in options) {
+    if (options.hasOwnProperty(key)) {
+      this[key] = options[key]
+    }
+  }
+}
+
+Precompiler.prototype.write = function (readTree) {
+  var self = this
+  quickTemp.makeOrRemake(this, '_tmpDestDir')
+  
+  //var destDir = path.join(this._tmpDestDir, self.outputFolder);
+  var destDir = path.join(self.outputFolder);
+    
+  var outputFiles = [];
+  
+  return readTree(this.inputTree).then(function (srcDir) {
+    var languages = fs.readdirSync(srcDir).filter(function(d){
+      var stats = fs.statSync(path.join(srcDir, d));
+      return d[0] != '.' && stats.isDirectory(); // Just exclude anything which starts with a . and isn't a directory
+    });
+
+    // setup the output-folder
+    mkdirp.sync(destDir);
+    
+    languages.forEach(function(language) {
+      var outputFile = path.join(destDir, language+".js");
+      var languageDir = path.join(srcDir, language);
+      
+      var inputFiles = helpers.multiGlob(['**/*.json'], {cwd: languageDir})
+
+      var output = {};
+
+      inputFiles.forEach(function(inputFile) {
+        var inputFilePath = path.join(languageDir, inputFile);
+        var valuePath = inputFile.substring(0,inputFile.indexOf('.json')).replace('/','.');
+        
+        var content = fs.readFileSync(inputFilePath, {encoding: 'utf8'});
+        
+        var tmp = JSON.parse(content);
+        if (valuePath === '_') {
+          output = _.merge(output, tmp);
+        } else {
+          setValue(output, valuePath, tmp);
+        }
+      });
+      
+      var string = JSON.stringify(output, null, "  ");
+      var result = string.replace(/:\s*["'](.*)["'](,?)/gi, function(match, p1, p2) {
+        var unescaped = unescape(p1);
+        var res = handlebars.precompile(unescaped);
+        return ": t(" + res.toString() + ")" + (p2 || ""); // We need to add back the colon and possibly the comma at the end
+      });
+
+      fs.writeFileSync(outputFile, result);
+      outputFiles.push(outputFile);
+    });
+  });
+}
+
+Precompiler.prototype.cleanup = function () {
+  quickTemp.remove(this, '_tmpDestDir')
+}
+
